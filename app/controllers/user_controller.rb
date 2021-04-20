bd0014613a21e3200622b75fc56c5887b96dbd37
Matt Budz <matt.budziosz@gmail.com>
2021-04-20 17:11:18 +0200

    add drop and render liquid show view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,20 @@
 class UserController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def index
+    @users = User.all
+  end
+
+  def show
+    file = File.read('./app/views/user/show.liquid')
+    template = Liquid::Template.parse(file)
+    drop = UserDrop.new(@user)
+    @liquid_template = template.render('user' => drop)
+  end
+
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
